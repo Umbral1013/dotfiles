f69620096c15d14c0df0a3c6c5d85dd298a6f0f5
Umbral1013 <Sixela121@hotmail.com>
2020-11-28 10:40:53 -0600

    XD

--- .esqueleto.py
+++ .esqueleto.py
@@ -1,12 +1,13 @@
-# BLOQUE DEL PROGRAMADOR
-#
-# NP = Aquí viene el nombre del programador
-# NP = Aquí viene el nombre del programa
-# Fecha = 
-# DP = Descripción del programa
-#
-#-------------------------------------
-#
-# Donde:
-#
-#-------------------------------------
+'''
+BLOQUE DEL PROGRAMADOR
+
+ NP = Aquí viene el nombre del programador
+ NP = Aquí viene el nombre del programa
+ Fecha = 
+ DP = Descripción del programa
+
+-------------------------------------
+
+ Donde:
+
+-------------------------------------'''
