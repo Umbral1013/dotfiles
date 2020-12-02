d9ec506fe4f268dde6b0fc754e916ccbca3cb8c2
Umbral1013 <Sixela121@hotmail.com>
2020-12-01 19:41:36 -0600

    Deleted backup files. Removed the function color from less (because it was not working).

--- zsh/.config/zsh/function.zsh
+++ zsh/.config/zsh/function.zsh
@@ -32,20 +32,3 @@
           LESS_TERMCAP_us=$'\e[01;32m' \
           command man "$@"
       }
-
-# Less in colors
-zmodload zsh/zpty
-
-pty() {
-	zpty pty-${UID} ${1+$@}
-	if [[ ! -t 1 ]];then
-		setopt local_traps
-		trap '' INT
-	fi
-	zpty -r pty-${UID}
-	zpty -d pty-${UID}
-}
-
-ptyless() {
-	pty $@ | less
-}
